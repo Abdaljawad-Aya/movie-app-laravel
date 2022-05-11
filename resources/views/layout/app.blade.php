642e121fdf556101a72598b7017481fc82d6e720
Abdaljawad-Ayah <abdaljawad.ayah94@gmail.com>
2022-05-11 11:57:45 +0300

    movie-api

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,6 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/css/app.css">
     <title>Movie App</title>
+
+    {{-- <livewire:styles > --}}
+    @livewireStyles
 </head>
 <body class="font-sans bg-blue-900 text-white">
     <nav class="border-b border-blue-800">
@@ -26,20 +29,8 @@
                             Shows</a></li></ul>
             </div>
             {{-- Search section --}}
-            <div class="flex items-center py-5">
-                <div class="relative">
-                    <input type="text" id="search-navbar" class="block p-2 pl-10 w-full text-gray-900 bg-gray-50  border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 
-                          focus:outline-none focus:shadow-outline
-                    " placeholder="Search...">
-                    <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none ">
-                        <svg class="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                clip-rule="evenodd"></path>
-                        </svg>
-                    </div>
-                </div>
+            <div class="flex flex-col md:flex-row items-center py-5">
+                <livewire:search-drop-down>
                 <div class="ml-4">
                     <a href="#">
                         <img src="{{ asset('img/avatar.webp') }}" alt="avatar" class="rounded-full w-8 h-8">
@@ -49,5 +40,8 @@
         </div>
     </nav>
     @yield('content')
+    {{-- <livewire:script> --}}
+    @livewireScripts
+
 </body>
 </html>
